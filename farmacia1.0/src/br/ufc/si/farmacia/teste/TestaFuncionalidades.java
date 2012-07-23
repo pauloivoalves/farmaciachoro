85a41a0edc1832050f999bfb83963ac90a11031c
mardsonferreira25 <mardsonferreira25@gmail.com>
2012-07-23 11:51:52 +0000

    dispensa paciente quase toda implementada,falta algumas informações de medicamento.

--- farmacia1.0/src/br/ufc/si/farmacia/teste/TestaFuncionalidades.java
+++ farmacia1.0/src/br/ufc/si/farmacia/teste/TestaFuncionalidades.java
@@ -1,5 +1,8 @@
 package br.ufc.si.farmacia.teste;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.ufc.si.farmacia.dao.PacienteDAO;
 import br.ufc.si.farmacia.interfaces.IPacienteDAO;
 import br.ufc.si.farmacia.model.Paciente;
@@ -8,10 +11,24 @@ public class TestaFuncionalidades {
  public static void main(String[] args) {
 	IPacienteDAO ipaciente = new PacienteDAO();
 	
-	Paciente paciente = new Paciente("marcos", 123456789L, "12345");
+	Paciente paciente = new Paciente("lucas", 123456789L, "1234567");
 	
-	ipaciente.inserirPaciente(paciente);
+	//ipaciente.inserirPaciente(paciente);
 	
+	/*
+	List<Paciente> lista = new ArrayList<Paciente>();
+	lista = ipaciente.listaTodosPacientes();
+	
+	for(Paciente elem:lista){
+		System.out.println(elem.getNome() + "|" + elem.getNumero_cartao_sus() + "|" + elem.getRg());
+	}
+	*/
+	
+	paciente = ipaciente.pacientePorId(2);
+	//System.out.println(paciente.getNome());
+	//paciente.setNome("jorge");
+	//ipaciente.atualizarPaciente(paciente);
+	//ipaciente.removerPaciente(paciente);
 	
 }
 }//fim da classe
