394618d6b5481df65826c293cbb068a760d6303e
pedrosiufc <pedrosiufc@gmail.com>
2012-08-06 01:02:39 +0000

    Adicionado ao projeto o crud do dispensar medicamento para unidade.

---
+++ farmacia1.0/src/br/ufc/si/farmacia/model/DispensaMedicamentoUnidade.java
@@ -0,0 +1,108 @@
+package br.ufc.si.farmacia.model;
+
+import java.util.Date;
+
+public class DispensaMedicamentoUnidade {
+
+	private int id;
+	private String tipoMedicamento;
+	private String tipoSaida;
+	private int qtdSaida;
+	private Date dataDispensa;
+	private Remedio remedio;
+	private Unidade unidade;
+	
+	
+	public DispensaMedicamentoUnidade() {
+		
+	}
+
+	public DispensaMedicamentoUnidade(int id, String tipoMedicamento,
+			String tipoSaida, int qtdSaida, Date dataDispensa, Remedio remedio,
+			Unidade unidade) {
+		super();
+		this.id = id;
+		this.tipoMedicamento = tipoMedicamento;
+		this.tipoSaida = tipoSaida;
+		this.qtdSaida = qtdSaida;
+		this.dataDispensa = dataDispensa;
+		this.remedio = remedio;
+		this.unidade = unidade;
+	}
+
+
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getTipoMedicamento() {
+		return tipoMedicamento;
+	}
+
+
+	public void setTipoMedicamento(String tipoMedicamento) {
+		this.tipoMedicamento = tipoMedicamento;
+	}
+
+
+	public String getTipoSaida() {
+		return tipoSaida;
+	}
+
+
+	public void setTipoSaida(String tipoSaida) {
+		this.tipoSaida = tipoSaida;
+	}
+
+
+	public int getQtdSaida() {
+		return qtdSaida;
+	}
+
+
+	public void setQtdSaida(int qtdSaida) {
+		this.qtdSaida = qtdSaida;
+	}
+
+
+	public Date getDataDispensa() {
+		return dataDispensa;
+	}
+
+
+	public void setDataDispensa(Date dataDispensa) {
+		this.dataDispensa = dataDispensa;
+	}
+
+
+	public Remedio getRemedio() {
+		return remedio;
+	}
+
+
+	public void setRemedio(Remedio remedio) {
+		this.remedio = remedio;
+	}
+
+
+	public Unidade getUnidade() {
+		return unidade;
+	}
+
+
+	public void setUnidade(Unidade unidade) {
+		this.unidade = unidade;
+	}
+		
+	
+	
+}
